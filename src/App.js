3727b32fd382c3b7ef83deb357211a057c2e1ced
Krishna2217 <rautkrishna2217@gmail.com>
2023-06-12 20:01:29 +0545

    Project completed

--- src/App.js
+++ src/App.js
@@ -1,17 +1,44 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
-import About from "./components/About";
+import { useState } from "react";
+// import About from "./components/About";
+import Alert from "./components/Alert";
 
 function App() {
+  const [mode,setMode] = useState('light');// whether dark mode is enable or not
+  const [alert,setAlert] = useState(null);
+
+  const showAlert = (message, type)=>{
+    setAlert({
+      msg:message,
+      type:type
+    })
+    setTimeout(() => {
+      setAlert(null)
+    }, 1500);
+  }
+  const toggleMode = ()=>{
+    if(mode==='light'){
+      setMode('dark');
+      document.body.style.backgroundColor= '#042743';
+      showAlert("Dark mode has been enable ", "success")
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor= 'white';
+      showAlert("Light mode has been enable ", "success")
+    }
+  }
   return (
     <>
-      <Navbar title="TextUtils2"/>
+      <Navbar title="TextUtils2" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert}/>
       <div className="container my-3"> 
-          <TextForm Heading ="Enter the text to analyse below"/> 
+          <TextForm showAlert={showAlert} Heading ="Enter the text to analyse below" mode={mode} /> 
           {/* we kept in container so that it wiil keep it in center using container class*/}
-          <About/>
+          {/* <About/> */}
       </div>
     </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -7,14 +7,17 @@ export default function TextForm(props) {
         // textChange("You have clicked on change change to upper")
         let newText = text.toUpperCase();
         textChange(newText);
+        props.showAlert("Converted to uppercase", "success");
     }
     const changeToLower =()=>{
         let newText = text.toLowerCase();
         textChange(newText);
+        props.showAlert("Converted to lowercase", "success");
     }
     const handleErase =()=>{
         let newText = '';
         textChange(newText);
+        props.showAlert("Text deleted", "success");
     }
     // const valueChanged = ()=>{ this will not allow to write in text area using event object
     //     console.log("Value changed");
@@ -23,37 +26,38 @@ export default function TextForm(props) {
         let text= document.getElementById('myBox');
         text.select();
         navigator.clipboard.writeText(text.value);
+        props.showAlert("Text copied to clipboard", "success");
     }
-
+    
     const valueChanged = (event)=>{
         // console.log("Value changed");
         textChange(event.target.value);
     }
 
-
-    const [text,textChange]=useState("Enter the text here");
+    const [text,textChange]=useState("");  
     // text= "new text"; wrong way to change text
     // setText("new text") correct way to change text
     return (
         <>
-            <div className="container">
+            <div className="container" style={{color:props.mode==='dark' ?'white':'#042743'}}>
                 <h1>{props.Heading}</h1>
                 <div className="mb-3">
                     
-                    <textarea className="form-control" id="myBox" value={text}  onChange={valueChanged} rows="8" ></textarea>
+                <textarea className="form-control" id="myBox" value={text}  onChange={valueChanged} style={{backgroundColor:props.mode==='dark' ?'grey':'white',color:props.mode==='dark' ?'white':'#042743'}} rows="8" ></textarea>
                 </div>
                 <button className="btn btn-primary mx-1" onClick={changeToUpper}>Convert to UpperCase</button>
                 <button className="btn btn-primary mx-1" onClick={changeToLower}>Convert to LowerCase</button>
                 <button className="btn btn-danger mx-1" onClick={handleErase}>Erase</button>
-                <button className="btn btn-danger mx-1" onClick={handleCopy}>Copy</button>
+                <button className="btn btn-primary mx-1" onClick={handleCopy}>Copy</button>
             </div>
-            <div className="container my-3"> 
+            <div className="container my-3" style={{color:props.mode==='dark' ?'white':'#042743'}}> 
                 <h2>Your text summary</h2>
                 <p>{text.split(" ").length} words and {text.length} characters</p>
                 <p>{0.008 * text.split(" ").length} Minutes to read</p>
                 <h2>Preview</h2>
-                <p>{text}</p>
+                <p>{text.length>0? text:"Enter something above to preview it."}</p>
             </div>
         </>
     )
 }
+
