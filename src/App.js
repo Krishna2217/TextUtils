abe973e9b898e283845b07618e001f8dfaaa8b66
Krishna2217 <rautkrishna2217@gmail.com>
2023-06-12 19:54:52 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
+import About from "./components/About";
 
 function App() {
   return (
     <>
       <Navbar title="TextUtils2"/>
-      
       <div className="container my-3"> 
           <TextForm Heading ="Enter the text to analyse below"/> 
           {/* we kept in container so that it wiil keep it in center using container class*/}
+          <About/>
       </div>
     </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -8,9 +8,22 @@ export default function TextForm(props) {
         let newText = text.toUpperCase();
         textChange(newText);
     }
+    const changeToLower =()=>{
+        let newText = text.toLowerCase();
+        textChange(newText);
+    }
+    const handleErase =()=>{
+        let newText = '';
+        textChange(newText);
+    }
     // const valueChanged = ()=>{ this will not allow to write in text area using event object
     //     console.log("Value changed");
     // }
+    const handleCopy = ()=>{
+        let text= document.getElementById('myBox');
+        text.select();
+        navigator.clipboard.writeText(text.value);
+    }
 
     const valueChanged = (event)=>{
         // console.log("Value changed");
@@ -22,13 +35,25 @@ export default function TextForm(props) {
     // text= "new text"; wrong way to change text
     // setText("new text") correct way to change text
     return (
-        <div>
-            <h1>{props.Heading}</h1>
-            <div className="mb-3">
-                
-                <textarea className="form-control" id="myBox" value={text}  onChange={valueChanged} rows="8" ></textarea>
+        <>
+            <div className="container">
+                <h1>{props.Heading}</h1>
+                <div className="mb-3">
+                    
+                    <textarea className="form-control" id="myBox" value={text}  onChange={valueChanged} rows="8" ></textarea>
+                </div>
+                <button className="btn btn-primary mx-1" onClick={changeToUpper}>Convert to UpperCase</button>
+                <button className="btn btn-primary mx-1" onClick={changeToLower}>Convert to LowerCase</button>
+                <button className="btn btn-danger mx-1" onClick={handleErase}>Erase</button>
+                <button className="btn btn-danger mx-1" onClick={handleCopy}>Copy</button>
             </div>
-            <button className="btn btn-primary" onClick={changeToUpper}>Convert to UpperCase</button>
-        </div>
+            <div className="container my-3"> 
+                <h2>Your text summary</h2>
+                <p>{text.split(" ").length} words and {text.length} characters</p>
+                <p>{0.008 * text.split(" ").length} Minutes to read</p>
+                <h2>Preview</h2>
+                <p>{text}</p>
+            </div>
+        </>
     )
 }
